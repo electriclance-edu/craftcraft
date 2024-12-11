930d55e0ef3f340c88177ab7a79e3031778d3aeb
lance libatique <70742357+electriclance-edu@users.noreply.github.com>
2024-12-11 20:05:55 +0800

    Item transfer

--- main.js
+++ main.js
@@ -33,12 +33,16 @@ var gameState = {
 
 class Inventory {
     slots;
-    constructor(rows,columns) {
-        this.slots = Array(rows).fill().map(()=>{
-            return Array(columns).fill().map(()=>{
-                return new InventorySlot()
-            })
-        })
+    constructor(name,rows,columns) {
+        this.name = name;
+        this.slots = [];
+        for (var r = 0; r < rows; r++) {
+            let row = [];
+            for (var c = 0; c < columns; c++) {
+                row.push(new InventorySlot(name,r + 1,c + 1));
+            } 
+            this.slots.push(row);
+        }
     }
     getSlot(r,c) {
         return this.slots[r - 1][c - 1]
@@ -46,7 +50,12 @@ class Inventory {
 }
 class InventorySlot {
     item;
-    constructor() {
+    constructor(inv,r,c) {
+        this.locationData = {
+            inventory:inv,
+            row:r,
+            column:c
+        }
         this.item = Item.empty
     }
     hasItem() {
@@ -54,17 +63,38 @@ class InventorySlot {
     }
     addItem(item) {
         this.item = item
+        this.updateView()
     }
     removeItem() {
         let removedItem = this.item
         this.item = Item.empty
-        return this.item;
+
+        this.updateView()
+
+        return removedItem;
     }
     transferItemTo(slot) {
         if (this.hasItem() && !slot.hasItem()) {
             let item = this.removeItem()
             slot.addItem(item)
         }
+    }
+    updateView() {
+        let elem = this.getElem();
+        if (this.item == Item.empty) {
+            elem.innerHTML = ""
+        } else {
+            elem.innerHTML = ""
+            let item = document.createElement("div")
+            item.classList.add("item")
+            elem.appendChild(item)
+        }
+    }
+    getElem() {
+        return document.getElementById(this.getId());
+    }
+    getId() {
+        return `${this.locationData.inventory}-${this.locationData.row}-${this.locationData.column}`
     }
 }
 class Item {
@@ -87,12 +117,14 @@ function onload() {
     initializeInventoryData()
     initializeInventoryElements()
     console.log(gameState)
+
+    gameState.mainInventory.getSlot(1,1).addItem(new Item("rock"))
 }
 
 function initializeInventoryData() {
-    gameState.craftingInventory = new Inventory(3,3)
-    gameState.mainInventory = new Inventory(3,9)
-    gameState.minionInventory = new Inventory(1,9)
+    gameState.craftingInventory = new Inventory("craftingInventory",3,3);
+    gameState.mainInventory = new Inventory("mainInventory",3,9);
+    gameState.minionInventory = new Inventory("minionInventory",1,9);
 }
 function initializeInventoryElements() {
     let slots = document.getElementsByClassName("itemSlot")
@@ -110,19 +142,15 @@ function inventorySlotManageMouseDown(e) {
 function inventorySlotManageMouseUp(e) {
     let data = getDataOfInventorySlotElement(e.target);
     selectedSlot.transferItemTo(gameState[data.inventory].getSlot(data.row,data.column))
-    console.log(data)
 }
 function getDataOfInventorySlotElement(elem) {
     // Return the inventory, row, and column of that slot.
-    let raw = elem.getAttribute('data-slot').split("-")
+    let raw = elem.id.split("-")
     return {
         inventory:raw[0],
         row:parseInt(raw[1]),
         column:parseInt(raw[2])
     }
-}
-function addItem(inventory,r,c) {
-    assert(["mainInventory","minionInventory","craftingInventory"].includes(inventory),`Invalid inventory ${inventory} passed into addItem().`)
 }
 
 const assert = function(condition, message = '') {
